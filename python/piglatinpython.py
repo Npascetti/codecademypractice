880559fb62d1a035a887bde47bda213dcd3aa0c0
Nick Pascetti <npascetti@cnm.edu>
2017-12-15 22:44:12 -0700

    pig latin python

--- python/piglatinpython.py
+++ python/piglatinpython.py
@@ -5,14 +5,20 @@ pyg = 'ay'
 
 ###prompt for input###
 original = raw_input('Enter a word:')
-###if block for formatting to PL, takes one word, no whitespace###
+###if block for formatting
 if len(original) > 0 and original.isalpha():
   print original
   word = original.lower()
   first = word[0]
   new_word = word + first + pyg
   new_word = new_word[1:len(new_word)]
-  new_word = new_word.title()
-  print new_word
+  ###logic to check if the first letter in input was uppercase###
+  if original[0] == original[0].upper():
+      new_word = new_word.title()
+      print new_word
+  else:
+      print new_word
 else:
   print 'statement is not a valid string'
+
+print "Does this count as a change for you atom?"
